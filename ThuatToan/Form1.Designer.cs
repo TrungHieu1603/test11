c54a0b6c92ba521821cd5857b0a360b9552b1ff0
Dương Trung Hiếu <hd16032000@gmail.com>
2024-05-05 13:10:16 +0700

    Add project files.

---
+++ ThuatToan/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace ThuatToan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtlbK = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_bangro = new System.Windows.Forms.TextBox();
+            this.txt_khoak1 = new System.Windows.Forms.TextBox();
+            this.txt_khoak2 = new System.Windows.Forms.TextBox();
+            this.txt_nhapm = new System.Windows.Forms.TextBox();
+            this.txt_bangma = new System.Windows.Forms.TextBox();
+            this.rdb_Ceasar = new System.Windows.Forms.RadioButton();
+            this.rdb_Affine = new System.Windows.Forms.RadioButton();
+            this.rdb_Vigenere = new System.Windows.Forms.RadioButton();
+            this.btn_mahoa = new System.Windows.Forms.Button();
+            this.btn_giaima = new System.Windows.Forms.Button();
+            this.btn_nhaplai = new System.Windows.Forms.Button();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập bản rõ P:";
+            // 
+            // txtlbK
+            // 
+            this.txtlbK.AutoSize = true;
+            this.txtlbK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlbK.Location = new System.Drawing.Point(82, 210);
+            this.txtlbK.Name = "txtlbK";
+            this.txtlbK.Size = new System.Drawing.Size(118, 20);
+            this.txtlbK.TabIndex = 0;
+            this.txtlbK.Text = "Nhập khóa k:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(82, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nhập m:";
+            // 
+            // txt_bangro
+            // 
+            this.txt_bangro.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_bangro.Location = new System.Drawing.Point(261, 77);
+            this.txt_bangro.Name = "txt_bangro";
+            this.txt_bangro.Size = new System.Drawing.Size(224, 22);
+            this.txt_bangro.TabIndex = 1;
+            // 
+            // txt_khoak1
+            // 
+            this.txt_khoak1.Location = new System.Drawing.Point(261, 210);
+            this.txt_khoak1.Name = "txt_khoak1";
+            this.txt_khoak1.Size = new System.Drawing.Size(100, 22);
+            this.txt_khoak1.TabIndex = 1;
+            // 
+            // txt_khoak2
+            // 
+            this.txt_khoak2.Location = new System.Drawing.Point(385, 210);
+            this.txt_khoak2.Name = "txt_khoak2";
+            this.txt_khoak2.Size = new System.Drawing.Size(100, 22);
+            this.txt_khoak2.TabIndex = 1;
+            // 
+            // txt_nhapm
+            // 
+            this.txt_nhapm.Location = new System.Drawing.Point(261, 254);
+            this.txt_nhapm.Name = "txt_nhapm";
+            this.txt_nhapm.Size = new System.Drawing.Size(100, 22);
+            this.txt_nhapm.TabIndex = 1;
+            // 
+            // txt_bangma
+            // 
+            this.txt_bangma.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_bangma.Location = new System.Drawing.Point(261, 293);
+            this.txt_bangma.Name = "txt_bangma";
+            this.txt_bangma.Size = new System.Drawing.Size(224, 22);
+            this.txt_bangma.TabIndex = 1;
+            // 
+            // rdb_Ceasar
+            // 
+            this.rdb_Ceasar.AutoSize = true;
+            this.rdb_Ceasar.BackColor = System.Drawing.SystemColors.Info;
+            this.rdb_Ceasar.Location = new System.Drawing.Point(16, 36);
+            this.rdb_Ceasar.Name = "rdb_Ceasar";
+            this.rdb_Ceasar.Size = new System.Drawing.Size(90, 24);
+            this.rdb_Ceasar.TabIndex = 2;
+            this.rdb_Ceasar.TabStop = true;
+            this.rdb_Ceasar.Text = "Ceasar";
+            this.rdb_Ceasar.UseVisualStyleBackColor = false;
+            this.rdb_Ceasar.CheckedChanged += new System.EventHandler(this.rdb_Ceasar_CheckedChanged);
+            // 
+            // rdb_Affine
+            // 
+            this.rdb_Affine.AutoSize = true;
+            this.rdb_Affine.BackColor = System.Drawing.SystemColors.Info;
+            this.rdb_Affine.Location = new System.Drawing.Point(140, 36);
+            this.rdb_Affine.Name = "rdb_Affine";
+            this.rdb_Affine.Size = new System.Drawing.Size(79, 24);
+            this.rdb_Affine.TabIndex = 2;
+            this.rdb_Affine.TabStop = true;
+            this.rdb_Affine.Text = "Affine";
+            this.rdb_Affine.UseVisualStyleBackColor = false;
+            this.rdb_Affine.CheckedChanged += new System.EventHandler(this.rdb_Affine_CheckedChanged);
+            // 
+            // rdb_Vigenere
+            // 
+            this.rdb_Vigenere.AutoSize = true;
+            this.rdb_Vigenere.BackColor = System.Drawing.SystemColors.Info;
+            this.rdb_Vigenere.Location = new System.Drawing.Point(258, 36);
+            this.rdb_Vigenere.Name = "rdb_Vigenere";
+            this.rdb_Vigenere.Size = new System.Drawing.Size(104, 24);
+            this.rdb_Vigenere.TabIndex = 2;
+            this.rdb_Vigenere.TabStop = true;
+            this.rdb_Vigenere.Text = "Vigenere";
+            this.rdb_Vigenere.UseVisualStyleBackColor = false;
+            this.rdb_Vigenere.CheckedChanged += new System.EventHandler(this.rdb_Vigenere_CheckedChanged);
+            // 
+            // btn_mahoa
+            // 
+            this.btn_mahoa.Location = new System.Drawing.Point(83, 359);
+            this.btn_mahoa.Name = "btn_mahoa";
+            this.btn_mahoa.Size = new System.Drawing.Size(75, 44);
+            this.btn_mahoa.TabIndex = 3;
+            this.btn_mahoa.Text = "Mã hóa";
+            this.btn_mahoa.UseVisualStyleBackColor = true;
+            this.btn_mahoa.Click += new System.EventHandler(this.btn_mahoa_Click_1);
+            this.btn_mahoa.MouseHover += new System.EventHandler(this.btn_mahoa_MouseHover);
+            // 
+            // btn_giaima
+            // 
+            this.btn_giaima.Location = new System.Drawing.Point(206, 359);
+            this.btn_giaima.Name = "btn_giaima";
+            this.btn_giaima.Size = new System.Drawing.Size(75, 44);
+            this.btn_giaima.TabIndex = 3;
+            this.btn_giaima.Text = "Giải mã";
+            this.btn_giaima.UseVisualStyleBackColor = true;
+            this.btn_giaima.Click += new System.EventHandler(this.btn_giaima_Click_1);
+            // 
+            // btn_nhaplai
+            // 
+            this.btn_nhaplai.Location = new System.Drawing.Point(324, 359);
+            this.btn_nhaplai.Name = "btn_nhaplai";
+            this.btn_nhaplai.Size = new System.Drawing.Size(75, 44);
+            this.btn_nhaplai.TabIndex = 3;
+            this.btn_nhaplai.Text = "Nhập lại";
+            this.btn_nhaplai.UseVisualStyleBackColor = true;
+            this.btn_nhaplai.Click += new System.EventHandler(this.btn_nhaplai_Click_1);
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Location = new System.Drawing.Point(454, 359);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(75, 44);
+            this.btn_thoat.TabIndex = 3;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
+            this.btn_thoat.Click += new System.EventHandler(this.btn_thoat_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(82, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Bản Mã:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdb_Ceasar);
+            this.groupBox1.Controls.Add(this.rdb_Affine);
+            this.groupBox1.Controls.Add(this.rdb_Vigenere);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(83, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(374, 75);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn hệ mã";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 523);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.btn_nhaplai);
+            this.Controls.Add(this.btn_giaima);
+            this.Controls.Add(this.btn_mahoa);
+            this.Controls.Add(this.txt_khoak2);
+            this.Controls.Add(this.txt_bangma);
+            this.Controls.Add(this.txt_nhapm);
+            this.Controls.Add(this.txt_khoak1);
+            this.Controls.Add(this.txt_bangro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtlbK);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtlbK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_bangro;
+        private System.Windows.Forms.TextBox txt_khoak1;
+        private System.Windows.Forms.TextBox txt_khoak2;
+        private System.Windows.Forms.TextBox txt_nhapm;
+        private System.Windows.Forms.TextBox txt_bangma;
+        private System.Windows.Forms.RadioButton rdb_Ceasar;
+        private System.Windows.Forms.RadioButton rdb_Affine;
+        private System.Windows.Forms.RadioButton rdb_Vigenere;
+        private System.Windows.Forms.Button btn_mahoa;
+        private System.Windows.Forms.Button btn_giaima;
+        private System.Windows.Forms.Button btn_nhaplai;
+        private System.Windows.Forms.Button btn_thoat;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ThuatToan/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ThuatToan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_mahoa_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void btn_giaima_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private string CaesarEncrypt(string plaintext, int shift)
+        {
+            StringBuilder encryptedText = new StringBuilder();
+
+            foreach (char c in plaintext)
+            {
+                if (char.IsLetter(c))
+                {
+                    char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                    char encryptedChar = (char)((((c - baseChar) + shift) % 26) + baseChar);
+                    encryptedText.Append(encryptedChar);
+                    txt_bangma.ForeColor = System.Drawing.Color.Green;
+                    
+                }
+                else
+                {
+                    encryptedText.Append(c);
+                }
+            }
+
+            return encryptedText.ToString();
+        }
+
+        private string CaesarDecrypt(string ciphertext, int shift)
+        {
+            txt_bangro.ForeColor = System.Drawing.Color.Red;
+
+            return CaesarEncrypt(ciphertext, 26 - shift);
+            
+        }
+
+        private string AffineEncrypt(string plaintext, int m, int b)
+        {
+            StringBuilder encryptedText = new StringBuilder();
+
+            foreach (char c in plaintext)
+            {
+                if (char.IsLetter(c))
+                {
+                    char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                    char encryptedChar = (char)((((m * (c - baseChar) + b) % 26) + 26) % 26 + baseChar);
+                    encryptedText.Append(encryptedChar);
+                }
+                else
+                {
+                    encryptedText.Append(c);
+                }
+            }
+
+            return encryptedText.ToString();
+        }
+
+        private string AffineDecrypt(string ciphertext, int m, int b)
+        {
+            int mInverse = Modinverse(m, 26); 
+            StringBuilder decryptedText = new StringBuilder();
+
+            foreach (char c in ciphertext)
+            {
+                if (char.IsLetter(c))
+                {
+                    char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                    char decryptedChar = (char)((((mInverse * (c - baseChar - b) + 26) % 26) + 26) % 26 + baseChar);
+                    decryptedText.Append(decryptedChar);
+                }
+                else
+                {
+                    decryptedText.Append(c);
+                }
+            }
+
+            return decryptedText.ToString();
+        }
+
+
+
+        private string VigenereEncrypt(string plaintext, string key)
+        {
+            StringBuilder encryptedText = new StringBuilder();
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                char c = plaintext[i];
+                char keyChar = key[i % key.Length];
+
+                if (char.IsLetter(c))
+                {
+                    char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                    int shift = char.ToUpper(keyChar) - 'A';
+                    char encryptedChar = (char)((((c - baseChar) + shift) % 26) + baseChar);
+                    encryptedText.Append(encryptedChar);
+                }
+                else
+                {
+                    encryptedText.Append(c);
+                }
+            }
+
+            return encryptedText.ToString();
+        }
+       
+        private string VigenereDecrypt(string ciphertext, string key)
+        {
+            StringBuilder decryptedText = new StringBuilder();
+
+            for (int i = 0; i < ciphertext.Length; i++)
+            {
+                char c = ciphertext[i];
+                char keyChar = key[i % key.Length];
+
+                if (char.IsLetter(c))
+                {
+                    char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                    int shift = char.ToUpper(keyChar) - 'A';
+                    char decryptedChar = (char)((((c - baseChar) - shift + 26) % 26) + baseChar);
+                    decryptedText.Append(decryptedChar);
+                }
+                else
+                {
+                    decryptedText.Append(c);
+                }
+            }
+
+            return decryptedText.ToString();
+        }
+      
+
+        private int Modinverse(int a, int m)
+        {
+            a = a % m;
+            for (int x = 1; x < m; x++)
+            {
+                if ((a * x) % m == 1)
+                {
+                    return x;
+                }
+            }
+            throw new Exception("Modular multiplicative inverse does not exist.");
+        }
+     
+        private void btn_thoat_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void rdb_Ceasar_CheckedChanged(object sender, EventArgs e)
+        {
+            txt_khoak2.Visible = false;
+            txt_nhapm.Visible = false;
+            txtlbK.Text = "Nhập Khóa K";
+            rdb_Ceasar.ForeColor = System.Drawing.Color.DarkRed;
+            rdb_Affine.ForeColor = System.Drawing.Color.Black;
+            rdb_Vigenere.ForeColor = System.Drawing.Color.Black;
+
+
+        }
+
+        private void rdb_Affine_CheckedChanged(object sender, EventArgs e)
+        {
+            txt_nhapm.Visible = false;
+            txt_khoak2.Visible = true;
+            txt_khoak1.Visible = true;
+            txtlbK.Text = "Nhập a b";
+            rdb_Ceasar.ForeColor = System.Drawing.Color.Black;
+            rdb_Affine.ForeColor = System.Drawing.Color.DarkRed;
+            rdb_Vigenere.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void rdb_Vigenere_CheckedChanged(object sender, EventArgs e)
+        {
+            txt_khoak2.Visible = false;
+            txt_nhapm.Visible = true;
+            txt_khoak1.Visible = true;
+            txtlbK.Text = "Nhập Khóa K";
+            rdb_Affine.ForeColor = System.Drawing.Color.Black;
+            rdb_Ceasar.ForeColor = System.Drawing.Color.Black;
+            rdb_Vigenere.ForeColor = System.Drawing.Color.DarkRed;
+
+        }
+
+        private void btn_nhaplai_Click_1(object sender, EventArgs e)
+        {
+            txt_bangro.Clear();
+            txt_khoak1.Clear();
+            txt_khoak2.Clear();
+            txt_bangma.Clear();
+            txt_nhapm.Clear();
+            rdb_Ceasar.Checked = false;
+            rdb_Affine.Checked = false;
+            rdb_Vigenere.Checked = false;
+        }
+
+        private void btn_thoat_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_mahoa_Click_1(object sender, EventArgs e)
+        {
+            string plaintext = txt_bangro.Text.ToUpper();
+            string key = txt_khoak1.Text.ToUpper();
+            string ciphertext = "";
+
+            if (rdb_Ceasar.Checked)
+            {
+                int shift = Convert.ToInt32(key);
+                ciphertext = CaesarEncrypt(plaintext, shift);
+            }
+            else if (rdb_Affine.Checked)
+            {
+                int m = Convert.ToInt32(key);
+                int b = Convert.ToInt32(txt_khoak2.Text);
+                ciphertext = AffineEncrypt(plaintext, m, b);
+            }
+            else if (rdb_Vigenere.Checked)
+            {
+                ciphertext = VigenereEncrypt(plaintext, key);
+            }
+
+            txt_bangma.Text = ciphertext;
+        }
+
+        private void btn_giaima_Click_1(object sender, EventArgs e)
+        {
+            string ciphertext = txt_bangma.Text.ToUpper();
+            string key = txt_khoak1.Text.ToUpper();
+            string plaintext = "";
+
+            if (rdb_Ceasar.Checked)
+            {
+                int shift = Convert.ToInt32(key);
+                plaintext = CaesarDecrypt(ciphertext, shift);
+            }
+            else if (rdb_Affine.Checked)
+            {
+                int m = Convert.ToInt32(key);
+                int b = Convert.ToInt32(txt_khoak2.Text);
+                plaintext = AffineDecrypt(ciphertext, m, b);
+            }
+            else if (rdb_Vigenere.Checked)
+            {
+                plaintext = VigenereDecrypt(ciphertext, key);
+            }
+
+            txt_bangro.Text = plaintext;
+        }
+
+        private void btn_mahoa_MouseHover(object sender, EventArgs e)
+        {
+           /* Random random = new Random();
+            int newX = random.Next(0, this.ClientSize.Width - btn_mahoa.Width);
+            int newY = random.Next(0, this.ClientSize.Height - btn_mahoa.Height);
+
+            newX = Math.Max(newX, 0);
+            newY = Math.Max(newY, 0);
+            newX = Math.Min(newX, this.ClientSize.Width - btn_mahoa.Width);
+            newY = Math.Min(newY, this.ClientSize.Height - btn_mahoa.Height);
+
+            btn_mahoa.Location = new Point(newX, newY);*/
+        }
+    }
+}
